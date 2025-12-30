2a98c5347e028b63130c72865059edec1a13731b
AlexTsamprounis <alex_tsamprounis@hotmail.com>
2025-12-31 00:13:20 +0200

    new models plus textarea modification

--- Test2.js
+++ Test2.js
@@ -1,1 +1,18 @@
+/* Textarea character counter */
 
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const input = document.querySelector("#formComments");
+    const remainingCharsDisplay = document.querySelector("#remainingChars");
+    const maxChars = 400;
+
+    input.addEventListener("input", function() {
+        const remainingChars = maxChars - input.value.length;
+        remainingCharsDisplay.textContent = remainingChars;
+
+    });
+
+    remainingCharsDisplay.innerHTML = maxChars;
+});
+
+/* Event listeners for form and input elements */
